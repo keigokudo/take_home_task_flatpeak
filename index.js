1b67c2cb9a2b3038be5e223a9d737964302e65c1
Keigo Kudo <keigokudo55@googlemail.com>
2025-10-01 17:01:55 +0100

    remove OTP_CODE

--- index.js
+++ index.js
@@ -6,10 +6,9 @@ require("dotenv").config();
 
 // Load environment variables from .env file
 const EMAIL = process.env.EMAIL;
-const OTP_CODE = process.env.OTP_CODE;
 
-if (!EMAIL || !OTP_CODE) {
-  console.error("Please set EMAIL and OTP_CODE in the .env file");
+if (!EMAIL) {
+  console.error("Please set EMAIL in the .env file");
   process.exit(1);
 }
 
