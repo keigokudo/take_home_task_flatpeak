1fa69e6629610424f726869e77d53756a2059020
Keigo Kudo <keigokudo55@googlemail.com>
2025-10-01 12:02:27 +0100

    access to the login endpoint

--- index.js
+++ index.js
@@ -9,3 +9,14 @@ if (!EMAIL || !OTP_CODE) {
   console.error("Please set EMAIL and OTP_CODE in the .env file");
   process.exit(1);
 }
+
+// Configuration
+const BASE_URL = "https://tazah1-dashboard.flatpeak.com";
+const LOGIN_ENDPOINT = `${BASE_URL}/api/trpc/auth.authenticateOtp?batch=1`;
+
+const client = axios.create({
+  baseURL: LOGIN_ENDPOINT,
+  timeout: 60000, // 60 seconds timeout
+  withCredentials: true, // to keep the jwt during the request
+});
+console.log("client", client);
